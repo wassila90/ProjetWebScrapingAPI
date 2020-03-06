a381ab77ccff5084d68dbaf5aa4ebe2f4f0efb7f
wassila <wassilayahiaoui90@hotmail.com>
2020-03-06 17:21:33 +0100

    installation packages et chargement libraries

---
+++ WassilaYAHIAOUI.R
@@ -0,0 +1,9 @@
+#USA (New castle et albany) dataset utilisant le webscrapin et une API qui recupére des informations pour chaque ville de l`USA  ###
+
+install.packages(c("polite","tidyverse","xml2","rvest","jsonlite"))
+library(polite)
+library(tidyverse)
+library(rvest)
+library(jsonlite)
+
+
